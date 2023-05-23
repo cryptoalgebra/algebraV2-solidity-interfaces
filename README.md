c99befe30b077e7cb8b8a408dd7944a439a2d6cc
Vladislav Volosnikov <Vladyan18@gmail.com>
2023-05-23 17:48:22 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Algebra V2 Core. Periphery and Tokenomics (farmings) interfaces
---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 0,
+      },
+      metadata: {
+        bytecodeHash: 'none',
+      },
+    },
+  }
+};
